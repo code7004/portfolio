eaf586b29b1ce51199d976047f510d5467ed637d
code7004 <code7004@gmail.com>
2026-02-12 16:45:24 +0900

    chore: css, font, normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>This is App.jsx</div>;
+  return (
+    <div>
+      <div>This is Portfolio</div>
+      <div>포트폴리오 </div>
+    </div>
+  );
 };
 
 export default App;
